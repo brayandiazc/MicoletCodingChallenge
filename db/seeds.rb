2360b8e6aee3ca50f9989ee9a1c3fa44a91ad32d
Brayan Diaz C <brayandiazc@gmail.com>
2022-02-01 20:41:56 -0500

    Doc: Seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+puts "Importing preferences..."
+CSV.foreach(Rails.root.join("db/seeds/preferences.csv"), headers: true) do |row|
+  Preference.create! do |preference|
+    preference.id = row[0]
+    preference.name = row[1]
+  end
+end
